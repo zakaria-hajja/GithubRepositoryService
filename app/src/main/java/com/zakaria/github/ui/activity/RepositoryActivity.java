483d9ca64dca53af30431efd2b4489085d977025
Zakaria <zakaria.hajja.1604@gmail.com>
2018-01-14 14:24:38 +0000

    Use fragment throw activity instead of using activity directly

--- app/src/main/java/com/zakaria/github/ui/activity/RepositoryActivity.java
+++ app/src/main/java/com/zakaria/github/ui/activity/RepositoryActivity.java
@@ -1,9 +1,11 @@
 package com.zakaria.github.ui.activity;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.zakaria.github.R;
+import com.zakaria.github.ui.fragment.RepositoryFragment;
 
 public class RepositoryActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class RepositoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository);
+
+        if(savedInstanceState == null){
+            final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment, RepositoryFragment.newInstance());
+            ft.commit();
+
+        }
     }
 }
--- app/src/main/java/com/zakaria/github/ui/fragment/RepositoryFragment.java
+++ app/src/main/java/com/zakaria/github/ui/fragment/RepositoryFragment.java
@@ -1,8 +1,44 @@
 package com.zakaria.github.ui.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zakaria.github.R;
+
 /**
  * Created by Zakaria on 14/01/2018.
  */
 
-public class RepositoryFragment {
+public class RepositoryFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    public static RepositoryFragment newInstance(){
+        return new RepositoryFragment();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_repository, container, false);
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
