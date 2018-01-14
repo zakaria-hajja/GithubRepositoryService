375bff1bb66a623611bd0adfafae14ec4377a3d0
Zakaria <zakaria.hajja.1604@gmail.com>
2018-01-14 15:08:28 +0000

    Setup RecyclerView     - Add Custom adapter     - Add list item custom UI     - Add Recycler to RepositoryFragment

---
+++ app/src/main/java/com/zakaria/github/ui/adapter/ListRepositoryAdapter.java
@@ -0,0 +1,54 @@
+package com.zakaria.github.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zakaria.github.databinding.RepositoryListItemBinding;
+import com.zakaria.github.model.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Zakaria on 14/01/2018.
+ */
+
+public class ListRepositoryAdapter extends RecyclerView.Adapter<ListRepositoryAdapter.ListRepositoryViewHolder> {
+
+    private final Context context;
+    private final List<Repository> repositories;
+
+    public ListRepositoryAdapter(Context context, List<Repository> repositories) {
+        this.context = context;
+        this.repositories = repositories;
+    }
+
+    public class ListRepositoryViewHolder extends RecyclerView.ViewHolder{
+        private final RepositoryListItemBinding binding;
+
+        public ListRepositoryViewHolder(RepositoryListItemBinding binding) {
+            super(binding.getRoot());
+        this.binding = binding;
+        }
+        public void bind(Repository repository) {
+            binding.setRepository(repository);
+        }
+
+    }
+    @Override
+    public ListRepositoryAdapter.ListRepositoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ListRepositoryViewHolder(RepositoryListItemBinding.inflate(LayoutInflater.from(context),parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(ListRepositoryAdapter.ListRepositoryViewHolder holder, int position) {
+        holder.bind(repositories.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return repositories.size();
+    }
+}
--- app/src/main/java/com/zakaria/github/ui/fragment/RepositoryFragment.java
+++ app/src/main/java/com/zakaria/github/ui/fragment/RepositoryFragment.java
@@ -1,24 +1,39 @@
 package com.zakaria.github.ui.fragment;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zakaria.github.R;
+import com.zakaria.github.databinding.FragmentRepositoryBinding;
+import com.zakaria.github.model.Owner;
+import com.zakaria.github.model.Repository;
+import com.zakaria.github.ui.adapter.ListRepositoryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Zakaria on 14/01/2018.
  */
 
 public class RepositoryFragment extends Fragment{
-
+    FragmentRepositoryBinding binding;
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        binding= DataBindingUtil.bind(getView());
+
+        List<Repository> repositories = new ArrayList<>();
+        binding.recycler.setAdapter(new ListRepositoryAdapter(this.getActivity(),repositories));
+        binding.recycler.setHasFixedSize(true);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     public static RepositoryFragment newInstance(){
@@ -29,8 +44,6 @@ public class RepositoryFragment extends Fragment{
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_repository, container, false);
     }
-
-
 
     @Override
     public void onAttach(Context context) {
