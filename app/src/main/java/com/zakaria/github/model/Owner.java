2a553de3041c1b7630d741a939139737ab57f59c
Zakaria <zakaria.hajja.1604@gmail.com>
2018-01-14 13:27:35 +0000

    Setup model layer

--- app/src/main/java/com/zakaria/github/model/Owner.java
+++ app/src/main/java/com/zakaria/github/model/Owner.java
@@ -1,8 +1,37 @@
 package com.zakaria.github.model;
 
+import java.io.Serializable;
+
 /**
  * Created by Zakaria on 14/01/2018.
  */
 
-public class Owner {
+public class Owner implements Serializable{
+    
+    private String name;
+    private String image;
+
+    public Owner() {
+    }
+
+    public Owner(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- app/src/main/java/com/zakaria/github/model/Repository.java
+++ app/src/main/java/com/zakaria/github/model/Repository.java
@@ -1,8 +1,47 @@
 package com.zakaria.github.model;
 
+import java.io.Serializable;
+
 /**
  * Created by Zakaria on 14/01/2018.
  */
 
-public class Repository {
+public class Repository implements Serializable {
+
+    private String name;
+    private String description;
+    private Owner owner;
+
+    public Repository() {
+    }
+
+    public Repository(String name, String description, Owner owner) {
+        this.name = name;
+        this.description = description;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
 }
