568a04b10fa56ea89c43a12527f07ad5d668802f
Zakaria <zakaria.hajja.1604@gmail.com>
2018-01-14 12:58:50 +0000

    initial commit

---
+++ app/src/main/java/com/github/zakaria/githubrepositoryservice/RepositoryActivity.java
@@ -0,0 +1,13 @@
+package com.github.zakaria.githubrepositoryservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
